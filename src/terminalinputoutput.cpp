29afb54ed345e258b08a89a1cca76a857a89a6e9
AVividLight <gibsonbethke@gmail.com>
2016-02-25 23:00:58 -0600

    Added compatibility for compilers that are not able to use C++11
    Follow the commented line’s instructions to upgrade to C++11/14.

--- src/terminalinputoutput.cpp
+++ src/terminalinputoutput.cpp
@@ -10,10 +10,10 @@ TerminalInputOutput::TerminalInputOutput()
 	vector<int> smallerPrimes;
 
 	cout << endl << "Enter a number: ";
-	cin >> inputNumber;
-	/*getline (cin, inputString);
-
-	inputNumber = stof(inputString);*/
+	cin >> inputNumber; // Delete if C++11 is available
+	
+	//getline (cin, inputString); // Uncomment if C++11 is available
+	//inputNumber = stof(inputString); // Uncomment if C++11 is available
 
 	CalculatePrimes calculatorFunction;
 	calculatorFunction.CalculateSmallerPrimes(inputNumber, smallerPrimes);
@@ -21,10 +21,13 @@ TerminalInputOutput::TerminalInputOutput()
 	cout << "All primes in " << inputString << endl;
 
 	int makeNewLine = 0;
-	for (int i : smallerPrimes)
+	//for (int i : smallerPrimes) // Uncomment if C++11 is available
+	int i = 0; // Delete if C++11 is available
+	while (i < smallerPrimes.size ()) // Delete if C++11 is available
 	{
 
-		cout << i << " ";
+		//cout << i << " "; // Uncomment if C++11 is available
+		cout << smallerPrimes[i] << " "; // Delete if C++11 is available
 		if (makeNewLine == 9)
 		{
 
@@ -33,6 +36,7 @@ TerminalInputOutput::TerminalInputOutput()
 		}
 
 		makeNewLine += 1;
+		i += 1; // Delete if C++11 is available
 	}
 
 	cout << endl;
